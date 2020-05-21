70df9ad1da84b69f20295a19ca1e8eabceba78a5
ziping3636 <2443113636@qq.com>
2020-05-21 22:06:55 +0800

    添加景区添加功能

--- travel-scenic-service/src/main/java/com.travel.scenic/controller/ScenicAreaController.java
+++ travel-scenic-service/src/main/java/com.travel.scenic/controller/ScenicAreaController.java
@@ -3,9 +3,11 @@ package com.travel.scenic.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.travel.scenic.config.ResultEntity;
 import com.travel.scenic.entity.ScenicArea;
 import com.travel.scenic.service.IScenicAreaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
@@ -27,11 +29,19 @@ public class ScenicAreaController {
     @Autowired
     private IScenicAreaService iScenicAreaService;
 
-    @RequestMapping("list")
+    @RequestMapping("/list")
     public Page selectAll(@RequestParam(defaultValue = "1") int current, @RequestParam(defaultValue = "10") int size, ScenicArea scenicArea) {
         Page page = new Page(current, size);
 
         return iScenicAreaService.selectAll(page, scenicArea);
+    }
+
+    @RequestMapping("/insert")
+    public ResultEntity insert(@RequestBody ScenicArea scenicArea) {
+        if (iScenicAreaService.save(scenicArea)) {
+            return ResultEntity.ok("添加景区成功");
+        }
+        return ResultEntity.error("添加景区失败");
     }
 }
 
